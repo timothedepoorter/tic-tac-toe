52b5259a6fd258c9af4da813da26ea478bf94b68
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2022-12-15 16:46:03 +0100

    optimize draw

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ function verifyEnd() {
     } else if (((buttons[2].innerHTML == buttons[4].innerHTML) && (buttons[2].innerHTML == buttons[6].innerHTML)) && (buttons[2].innerHTML == "X" || buttons[2].innerHTML == "O")) {
         alert(`Le joueur ${buttons[2].innerHTML} remporte la partie`);
     } else if ((buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O") &&
-    (buttons[1].innerHTML == "X" || buttons[1].innerHTML == "O") &&
-    (buttons[2].innerHTML == "X" || buttons[2].innerHTML == "O") &&
-    (buttons[3].innerHTML == "X" || buttons[3].innerHTML == "O") &&
-    (buttons[4].innerHTML == "X" || buttons[4].innerHTML == "O") &&
-    (buttons[5].innerHTML == "X" || buttons[5].innerHTML == "O") && 
-    (buttons[6].innerHTML == "X" || buttons[6].innerHTML == "O") &&
-    (buttons[7].innerHTML == "X" || buttons[7].innerHTML == "O") &&
-    (buttons[8].innerHTML == "X" || buttons[8].innerHTML == "O"))  {
-        alert("Egalité entre les deux joueurs !")
+               (buttons[1].innerHTML == "X" || buttons[1].innerHTML == "O") &&
+               (buttons[2].innerHTML == "X" || buttons[2].innerHTML == "O") &&
+               (buttons[3].innerHTML == "X" || buttons[3].innerHTML == "O") &&
+               (buttons[4].innerHTML == "X" || buttons[4].innerHTML == "O") &&
+               (buttons[5].innerHTML == "X" || buttons[5].innerHTML == "O") && 
+               (buttons[6].innerHTML == "X" || buttons[6].innerHTML == "O") &&
+               (buttons[7].innerHTML == "X" || buttons[7].innerHTML == "O") &&
+               (buttons[8].innerHTML == "X" || buttons[8].innerHTML == "O")) {
+               alert("Egalité entre les deux joueurs !")
     };
 };
 
