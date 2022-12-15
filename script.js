c7d68074a9c2ebbce5f94dfa09b839f2f4f57585
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2022-12-15 16:35:10 +0100

    create another function

--- script.js
+++ script.js
@@ -8,32 +8,33 @@ function handleCase() {
     if (this.classList.contains("blocked")) {
         console.log("Vous ne pouvez pas cliquer sur cette case.");
     } else {
-    (turn) ? this.innerHTML = "X" : this.innerHTML = "O";
-    turn = !turn;
-    this.classList.replace("case", "blocked");
+        (turn) ? this.innerHTML = "X" : this.innerHTML = "O";
+        turn = !turn;
+        this.classList.replace("case", "blocked");
     }; 
+};
+
+function verifyEnd() {
     if (((buttons[0].innerHTML == buttons[1].innerHTML) && (buttons[0].innerHTML == buttons[2].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
     } else if (((buttons[3].innerHTML == buttons[4].innerHTML) && (buttons[3].innerHTML == buttons[5].innerHTML)) && (buttons[3].innerHTML == "X" || buttons[3].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[3].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[3].innerHTML} remporte la partie`);
     } else if (((buttons[6].innerHTML == buttons[7].innerHTML) && (buttons[6].innerHTML == buttons[8].innerHTML)) && (buttons[6].innerHTML == "X" || buttons[6].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[6].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[6].innerHTML} remporte la partie`);
     } else if (((buttons[0].innerHTML == buttons[3].innerHTML) && (buttons[0].innerHTML == buttons[6].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
     } else if (((buttons[1].innerHTML == buttons[4].innerHTML) && (buttons[1].innerHTML == buttons[7].innerHTML)) && (buttons[1].innerHTML == "X" || buttons[1].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[1].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[1].innerHTML} remporte la partie`);
     } else if (((buttons[2].innerHTML == buttons[5].innerHTML) && (buttons[2].innerHTML == buttons[8].innerHTML)) && (buttons[2].innerHTML == "X" || buttons[8].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[2].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[2].innerHTML} remporte la partie`);
     } else if (((buttons[0].innerHTML == buttons[4].innerHTML) && (buttons[0].innerHTML == buttons[8].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+        alert(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
     } else if (((buttons[2].innerHTML == buttons[4].innerHTML) && (buttons[2].innerHTML == buttons[8].innerHTML)) && (buttons[2].innerHTML == "X" || buttons[2].innerHTML == "O")) {
-        console.log(`Le joueur ${buttons[2].innerHTML} remporte la partie`); 
+        alert(`Le joueur ${buttons[2].innerHTML} remporte la partie`);
     };
 };
 
 for (let button of buttons) {
     button.addEventListener("click", handleCase);
+    button.addEventListener("click", verifyEnd); 
 };
-
-
-
