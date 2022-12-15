f91fa51a906b6bc9b710ff78e4fa97e1162bf877
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2022-12-15 16:28:14 +0100

    add condition to win

--- script.js
+++ script.js
@@ -11,10 +11,29 @@ function handleCase() {
     (turn) ? this.innerHTML = "X" : this.innerHTML = "O";
     turn = !turn;
     this.classList.replace("case", "blocked");
-    };  
+    }; 
+    if (((buttons[0].innerHTML == buttons[1].innerHTML) && (buttons[0].innerHTML == buttons[2].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+    } else if (((buttons[3].innerHTML == buttons[4].innerHTML) && (buttons[3].innerHTML == buttons[5].innerHTML)) && (buttons[3].innerHTML == "X" || buttons[3].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[3].innerHTML} remporte la partie`);
+    } else if (((buttons[6].innerHTML == buttons[7].innerHTML) && (buttons[6].innerHTML == buttons[8].innerHTML)) && (buttons[6].innerHTML == "X" || buttons[6].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[6].innerHTML} remporte la partie`);
+    } else if (((buttons[0].innerHTML == buttons[3].innerHTML) && (buttons[0].innerHTML == buttons[6].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+    } else if (((buttons[1].innerHTML == buttons[4].innerHTML) && (buttons[1].innerHTML == buttons[7].innerHTML)) && (buttons[1].innerHTML == "X" || buttons[1].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[1].innerHTML} remporte la partie`);
+    } else if (((buttons[2].innerHTML == buttons[5].innerHTML) && (buttons[2].innerHTML == buttons[8].innerHTML)) && (buttons[2].innerHTML == "X" || buttons[8].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[2].innerHTML} remporte la partie`);
+    } else if (((buttons[0].innerHTML == buttons[4].innerHTML) && (buttons[0].innerHTML == buttons[8].innerHTML)) && (buttons[0].innerHTML == "X" || buttons[0].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[0].innerHTML} remporte la partie`);
+    } else if (((buttons[2].innerHTML == buttons[4].innerHTML) && (buttons[2].innerHTML == buttons[8].innerHTML)) && (buttons[2].innerHTML == "X" || buttons[2].innerHTML == "O")) {
+        console.log(`Le joueur ${buttons[2].innerHTML} remporte la partie`); 
+    };
 };
 
 for (let button of buttons) {
     button.addEventListener("click", handleCase);
 };
+
+
 
