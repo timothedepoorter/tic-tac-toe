255bb12d228c7a72218620547dbe6512b1a30286
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2022-12-15 14:31:38 +0100

    add disable

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ var buttons = document.querySelectorAll(".case");
 console.log(buttons);
 
 var turn = true;
+var i = 1;
 
 function handleCase() {
-    (turn) ? this.textContent = "X" : this.textContent = "O";
-    (turn) ? turn=false : turn = true;
-    console.log(turn)
+    if (i>9) {
+        buttons = buttons.disabled;
+    } else {
+        (turn) ? this.textContent = "X" : this.textContent = "O";
+        (turn) ? turn=false : turn = true;
+        console.log(turn);
+        i++;
+    };
 };
 
 for (let button of buttons) {
     button.addEventListener("click", handleCase);
 };
+
